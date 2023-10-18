6fa893e9755135db9ff983f978060c309bb2d5c4
Dina Butko <butkodina@gmail.com>
2023-10-18 18:36:00 -0400

    Update file_ops.py

--- Py_modules/file_ops.py
+++ Py_modules/file_ops.py
@@ -12,5 +12,9 @@ with open("myfile.txt",'r') as f:
     list2 = f.read().split("\n")
     print(list2)
 
+print("""Using with Option but with split""")
+with open("myfile.txt",'r') as f:
 
+    for line in f:
+        print(line.strip())
 
